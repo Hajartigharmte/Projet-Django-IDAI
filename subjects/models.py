ca10c43c98f129728affb2a1450c54969d9db6ed
ararou-israe <israeararou04@gmail.com>
2026-04-01 03:48:24 +0000

    fixing subjects templates

--- subjects/models.py
+++ subjects/models.py
@@ -5,13 +5,13 @@ from department.models import Department
 class Subject(models.Model):
     subject_name = models.CharField(max_length=255)
     
-    # Relation avec le professeur (déjà terminé)
+   
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='subjects')
     
     
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     
-    # Dates de création (toujours utile pour un projet pro)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
